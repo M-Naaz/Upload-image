8db210f28d65a4faef4464f85a4bef8add5ddc07
M-Naaz <84713195+M-Naaz@users.noreply.github.com>
2021-06-04 18:00:32 +0530

    Add files via upload

---
+++ upload.js
@@ -0,0 +1,27 @@
+const util = require('util');
+const multer = require("multer");
+const GridFsStorage = require("multer-gridfs-storage");
+
+
+const storage = new GridFsStorage({
+    url:"mongodb://localhost:27017/bezkoder_files_db",
+    options: { useUrlParser: true, useUnifiedTopology: true },
+    file: (req, file) => {
+        const match = ["image/png", "image/jpeg"];
+
+        if (match.indexOf(file.mimetype) === -1) {
+            const filename = `${Date.now()}-bezkoder-${file.originalname}`;
+            return filename;
+          }
+      
+          return {
+            bucketName: "photos",
+            filename: `${Date.now()}-bezkoder-${file.originalname}`
+          };
+        }
+      });
+      
+      var uploadFile = multer({ storage: storage }).single("file");
+      var uploadFilesMiddleware = util.promisify(uploadFile);
+      module.exports = uploadFilesMiddleware;
+    
